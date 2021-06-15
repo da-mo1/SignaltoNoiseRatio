2a5ddad89eafb387bfb1ae0a386d6b5a043b6f2f
da-mo1 <85586550+da-mo1@users.noreply.github.com>
2021-06-15 14:00:17 -0400

    added extensions

--- docs-src/source/conf.py
+++ docs-src/source/conf.py
@@ -27,7 +27,12 @@ author = 'D. Moraes'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['myst_parser',
+                'sphinx_rtd_theme',
+                'sphinx.ext.autodoc',
+                'sphinx.ext.autosummary',
+                'nbsphinx',
+                'nbsphinx_link'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
